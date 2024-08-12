6cc2a15f27f4924a53628b442a7969ed4b3908db
Madhusmita Pradhan <pmadhusmita63@gmail.com>
2024-08-12 20:19:23 +0530

    navbar

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Navbar from "./Components/Navbar/Navbar";
+
+const App = () => {
+  return(
+     <main className="overflow-x-hidden bg-white dark:bg-black">
+       <Navbar  />
+       
+    </main>
+);
+};
+
+export default App;
