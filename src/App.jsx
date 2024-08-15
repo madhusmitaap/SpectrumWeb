3b55e5aae1ba0bfa653d1b0a253dbdca5d31e1ad
ankitasahu2911 <145644574+ankitasahu2911@users.noreply.github.com>
2024-08-15 13:31:27 +0530

    leads

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import Navbar from "./Components/Navbar/Navbar";
+// import Project from "./Components/Project/Project";
+import Leads from "./Components/Leads/Leads";
+
 
 const App = () => {
   return(
      <main className="overflow-x-hidden bg-white dark:bg-black">
        <Navbar  />
-       
+       {/* <Project/> */}
+       <Leads/>
     </main>
 );
 };
