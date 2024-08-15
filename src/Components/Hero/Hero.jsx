1c6ebdbb260541b763f651283a4a415bc96c61e4
Madhusmita Pradhan <pmadhusmita63@gmail.com>
2024-08-15 11:46:14 +0530

    added hero page

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import HeroJpg from "../../assets/Hero.jpg";
+import Robo from "../../assets/robo1.png";
 import { BiPlayCircle } from "react-icons/bi";
 
 const Hero = () => {
@@ -7,15 +7,31 @@ const Hero = () => {
     <div className="py-12 sm:py-0 dark:bg-black
       dark:text-white duration-300 overflow-hidden">
         <div className="container min-h-[700px] flex relative">
-           <div className="grid">
+           <div className="grid grid-cols-1 sm:grid-cols-2  gap-6 place items-center relative z-10">
+
+          {/*hero text section*/}
+          <div className="order-2 sm:order-1 space-y-5 lg:pr-20">
+             <h1 className="text-4xl font-semibold">
+              SPECTRUM {" "}</h1>
+                <h1 className="text-4xl font-semibold">
+              <span className="bg-clip-text text-transparent bg-gradient-to-r
+               from-primary to-secondary">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero, ut!
+               </span>
+            </h1>
+          </div>
 
             {/*image section*/}
-            {/*hero text section*/}
-
-
-
-           </div>
+            <div className="order-1 sm:order-2" >
+              <img class="" src= {Robo} alt="" />
             </div>
+          
+           </div>
+           {/*Animated blob*/}
+           <div className="h-[300px] w-[300px] bg-gradient-to-r
+            from-primary to-secondary rounded-full
+            absolute top-0 left-0 blur-3xl animated-wrapper"></div>
+          </div>
     </div>
       
     </>;
