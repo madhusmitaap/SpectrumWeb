16d089a074c358576e2908312c48317664e1bc80
ankitasahu2911 <145644574+ankitasahu2911@users.noreply.github.com>
2025-02-27 14:09:59 +0530

    project_updates

--- src/Components/Project/Project.jsx
+++ src/Components/Project/Project.jsx
@@ -1,74 +1,41 @@
 import React from "react";
 import "./Project.css";
 import Project_1 from "../../assets/Project_1.jpg";
-import icon from "../../assets/icon.webp";
-const project = () => {
+import SID from "../../assets/SID.jpg";
+
+const Project = () => {
+  const projects = [
+    { image: SID, link: "https://github.com/Sarans13/SPECTRUM-SID-2023.git" },
+    { image: Project_1, link: "https://github.com/dashboard" },
+    { image: Project_1, link: "https://github.com/dashboard" },
+    { image: Project_1, link: "https://github.com/dashboard" },
+  ];
+
   return (
-    <div className="project">
+    <div className="project-section">
       <div className="project-title">
         <h1>PROJECTS</h1>
-        </div>
-          <div className="project-container">
-            <div className="project1">
-              <img src={Project_1} alt="" />
-              
-              <p className="project-detail">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Est, quaerat.</p>
-              <a className="a-link" href="https://github.com/dashboard">
-              <button className="btn">Visit</button>
-              </a>
-            </div>
+      </div>
 
-            <div className="project1">
-              <img src={Project_1} alt="" />
-              <p className="project-detail">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Est, quaerat.</p>
-              <a className="a-link" href="https://github.com/dashboard">
-              <button className="btn">Visit</button>
-              </a>
-            </div>
-
-            <div className="project1">
-              <img src={Project_1} alt="" />
-              <p className="project-detail">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Est, quaerat.</p>
-              <a className="a-link" href="https://github.com/dashboard">
-              <button className="btn">Visit</button>
-              </a>
-            </div>
-
-             <div className="project1">
-              <img src={Project_1} alt="" />
-              <p className="project-detail">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Est, quaerat.</p>
-              <a className="a-link" href="https://github.com/dashboard">
-              <button className="btn">Visit</button>
-              </a>
-            </div>
-
-            {/*<div className="project1">
-              <img src={Project_1} alt="" />
-              <p className="project-detail">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Est, quaerat.</p>
-              <a className="a-link" href="https://github.com/dashboard">
-              <button className="btn">Visit</button>
-              </a>
-            </div>
-
-            <div className="project1">
-              <img src={Project_1} alt="" />
-              <p className="project-detail">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Est, quaerat.</p>
-              <a className="a-link" href="https://github.com/dashboard">
-              <button className="btn">Visit</button>
-              </a>
-            </div> 
-            */}
+      <div className="project-container">
+        {projects.map((project, index) => (
+          <div className="project1" key={index}>
+            <img src={project.image} alt={`Project ${index + 1}`} className="project-image" />
+            <p className="project-detail">
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Est, quaerat.
+            </p>
+            <a className="a-link" href={project.link}>
+              <button className="project-btn">Visit</button>
+            </a>
           </div>
-          <div style={{ display: 'flex', justifyContent: 'center' }}>
-          <div className="flex gap-6">
-          <button className="primary-btn" ><b>Show More</b></button>
-          
-          </div>
-          </div>
-        </div>
-      
-    
+        ))}
+      </div>
+
+      <div className="btn-container">
+        <button className="show-more-btn"><b>Show More</b></button>
+      </div>
+    </div>
   );
 };
 
-export default project;
+export default Project;
